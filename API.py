9befa5e983fa0a9b93f0940cdbbc84e9599c0af7
ZEKRIFA Abdelmoumen <zekrifa.abdelmoumen@gmail.com>
2022-09-04 23:14:22 +0200

    the API work perfectly in local, and we added data_client.csv and finalized_model.sav

--- API.py
+++ API.py
@@ -1,18 +1,44 @@
 from flask import Flask
 from flask_restful import Api, Resource
+import pandas as pd
+import pickle as pc
+import numpy as np
+import shap
 
 app = Flask(__name__)
 api = Api(app)
 
+filename = 'finalized_model.sav'
+loaded_model = pc.load(open(filename, 'rb'))
+explainer = shap.TreeExplainer(loaded_model)
+df = pd.read_csv('data_client.csv')
+feats = [f for f in df.columns if f != "SK_ID_CURR"]
 
-class Hello_world(Resource):
-    def get(self, n):
-        a = n*n
-        return {"number": a
+
+class Score(Resource):
+    def get(self, id_curr):
+        x = df[df['SK_ID_CURR'] == id_curr][feats]
+        proba = loaded_model.predict_proba(x)[:, 1]
+        exp_value = explainer.shap_values(x)[1]
+        exp_value = np.reshape(exp_value, len(feats))
+        exp_value = pd.DataFrame(exp_value, index=feats, columns=['not_risky'])
+        impact = exp_value
+        impact['impact'] = impact['not_risky'].apply(lambda i: abs(i))
+        impact = impact.sort_values(by=['impact'], ascending=False).head(10)[["not_risky"]]
+        exp_value = exp_value.to_dict()['not_risky']
+
+        feat_imp = list(impact.to_dict()['not_risky'].keys())
+        index = pd.Series([0])
+        x = x[feat_imp].set_index(index)
+        return {"data": {"SK_ID_CURR": id_curr,
+                         "proba": float(proba),
+                         "impact": exp_value,
+                         "detail": x.T.to_dict()[0]}
+
                 }
 
 
-api.add_resource(Hello_world, "/hello_world/<int:n>")
+api.add_resource(Score, "/score/<int:id_curr>")
 
 if __name__ == "__main__":
     app.run(debug=True)
