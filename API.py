5976acc963e262eade2d3e49a6d2e4f20eb7cb32
ZEKRIFA Abdelmoumen <zekrifa.abdelmoumen@gmail.com>
2022-09-05 19:30:41 +0200

    test without shap

--- API.py
+++ API.py
@@ -10,7 +10,7 @@ api = Api(app)
 
 filename = 'finalized_model.sav'
 loaded_model = pc.load(open(filename, 'rb'))
-explainer = shap.TreeExplainer(loaded_model)
+#explainer = shap.TreeExplainer(loaded_model)
 df = pd.read_csv('data_client.csv')
 feats = [f for f in df.columns if f != "SK_ID_CURR"]
 
@@ -19,22 +19,27 @@ class Score(Resource):
     def get(self, id_curr):
         x = df[df['SK_ID_CURR'] == id_curr][feats]
         proba = loaded_model.predict_proba(x)[:, 1]
-        exp_value = explainer.shap_values(x)[1]
-        exp_value = np.reshape(exp_value, len(feats))
-        exp_value = pd.DataFrame(exp_value, index=feats, columns=['not_risky'])
-        impact = exp_value
-        impact['impact'] = impact['not_risky'].apply(lambda i: abs(i))
-        impact = impact.sort_values(by=['impact'], ascending=False).head(10)[["not_risky"]]
-        exp_value = exp_value.to_dict()['not_risky']
+#        exp_value = explainer.shap_values(x)[1]
+#        exp_value = np.reshape(exp_value, len(feats))
+#        exp_value = pd.DataFrame(exp_value, index=feats, columns=['not_risky'])
+#        impact = exp_value
+#        impact['impact'] = impact['not_risky'].apply(lambda i: abs(i))
+#        impact = impact.sort_values(by=['impact'], ascending=False).head(10)[["not_risky"]]
+#        exp_value = exp_value.to_dict()['not_risky']
 
-        feat_imp = list(impact.to_dict()['not_risky'].keys())
-        index = pd.Series([0])
-        x = x[feat_imp].set_index(index)
+#        feat_imp = list(impact.to_dict()['not_risky'].keys())
+#        index = pd.Series([0])
+#        x = x[feat_imp].set_index(index)
+        """
         return {"data": {"SK_ID_CURR": id_curr,
                          "proba": float(proba),
                          "impact": exp_value,
                          "detail": x.T.to_dict()[0]}
-
+                } 
+        """
+        return {"data": {"SK_ID_CURR": id_curr,
+                         "proba": float(proba),
+                         }
                 }
 
 
